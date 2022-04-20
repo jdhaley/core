e03f779073cd1f229b01146d63f24e89d053da45
jdh <jdhaley@gmail.com>
2022-04-20 09:58:05 -0400

    statement devt

--- devt/expr/statement.ts
+++ devt/expr/statement.ts
@@ -1,66 +1,56 @@
 import {Value, Type} from "../../api/value.js";
 import {EMPTY} from "../../base/data.js";
-import {Compilable, Scope} from "../../base/compiler.js";
-import {Markup} from "../../api/model.js";
+import {Scope} from "../../base/compiler.js";
 import lex from "./lexer.js";
 import parse from "./parser.js";
-import { Decl } from "../../../lang/compiler/compiler.js";
 
-type state = "initialized" | "compiling" | "compiled";
+const COMPILING = Object.freeze(Object.create(null));
+
 export class Statement  {
 	constructor(source: Element, parent?: Statement) {
 		this.source = source;
 		this.parent = parent;
 		this.content = source.children.length ? [] : EMPTY.array as any[];
 	}
-	#state: state;
 	#value: Value;
 	readonly source: Element;
 	readonly parent: Statement;
 	readonly content: Statement[];
 	
-	get comment() {
-		return this.source.getElementsByTagName("section").item(0);
+	get note() {
+		return this.source.getElementsByTagName("note").item(0);
 	}
 	get scope() {
 		return this.parent?.scope || null;
 	}
-
-	compile() {
-		switch (this.#state) {
-			case "compiling":
-				throw new Error("compilation cycle");
-			case "compiled":
-				return;
-			default:
-				break;
+	get value() {
+		if (this.#value === undefined) {
+			this.#value = COMPILING;
+			this.#value = this.compile();
 		}
-		for (let stmt of this.content) {
-			stmt.compile();
-		}
-		let value = this.source.getAttribute("value") || "";
-		let compilable = parse(lex(value));
-		this.#value = compilable.compile(this.scope);
+		return this.#value;
 	}
 	initialize() {
 		let source = this.source;
-		// this.source = lex(source.getAttribute("value") || "");
-		// this.expr = parse(stmt.source);
-
-		//if (stmt instanceof Declaration) ...
 		for (let child of source.children) {
 			if (child.nodeName == "s") {
-				let stmt = child.getAttribute("key")
-					? new Declaration(child, this)
-				 	: new Statement(child, this);
+				let stmt = this.createChild(child);
 				this.content.push(stmt);
 				stmt.initialize();
+			} else if (child.nodeName == "note") {
 			} else {
 				let err = new Statement(child, this);
 				this.scope.notice("error", `invalid child node "<${child.nodeName}>"`, err);
 			}
 		}
-		this.#state = "initialized";
+	}
+
+	private createChild(child: Element) {
+		if (child.getAttribute("key")) return new Declaration(child, this);
+		if (child.getAttribute("keyword")) return new KeywordStatement(child, this);
+		return new ExpressionStatement(child, this);
+	}
+	protected compile(): any {
 	}
 }
 export class Module extends Statement {
@@ -84,4 +74,20 @@ export class Declaration extends Statement {
 	facets: string[];
 	key: string;
 	type: Type;
+}
+
+export class KeywordStatement extends Statement {
+	compile(): any {
+		for (let stmt of this.content) {
+			if (stmt.value == COMPILING) {
+				throw new Error("compiling cycle");
+			}
+		}
+		let value = this.source.getAttribute("value") || "";
+		let compilable = parse(lex(value));
+		return compilable.compile(this.scope);
+	}
+}
+
+export class ExpressionStatement extends Statement {
 }
