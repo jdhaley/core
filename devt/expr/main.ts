87dde5dac578115b4d837a450d8c6f90fca63129
jdh <jdhaley@gmail.com>
2022-04-25 08:16:12 -0400

    statement.ts - implementation of Statement

--- devt/expr/main.ts
+++ devt/expr/main.ts
@@ -1,2 +1,2 @@
-import {Statement} from "./statement.js";
+import {Statement} from "../../base/compiler";
 import {Transformer} from "../../api/transform";
--- devt/expr/statement copy.ts
+++ devt/expr/statement copy.ts
@@ -1,43 +1,33 @@
 import {Value, Type} from "../../api/value.js";
-import {Scope, Statement} from "../../base/compiler.js";
-import {Impure, Pure} from "../../base/pure.js";
 import {EMPTY} from "../../base/data.js";
-
+import {Scope, Statement} from "../../base/compiler.js";
 import lex from "./lexer.js";
 import parse from "./parser.js";
+import { Impure, Pure } from "../../base/pure.js";
 
-export class Stmt extends Statement {
-	protected compile(): Value {
-		let value = this.source.getAttribute("value") || "";
-		if (!value) return;
-		let compilable = parse(lex(value));
-		return compilable.compile(this.scope);
-	}
-	initialize() {
-		let source = this.source;
-		for (let child of source.children) {
-			if (child.nodeName == "s") {
-				let stmt = createStatement(this, child);
-				this.content.push(stmt);
-				stmt.initialize();
-			} else if (child.nodeName == "note") {
-			} else {
-				this.scope.notice("error", `invalid child node "<${child.nodeName}>"`);
-			}
-		}
-	}
-}
-function createStatement(parent: Stmt, child: Element): Stmt {
-	if (child.getAttribute("key")) return new Declaration(child, parent);
-	if (child.getAttribute("keyword")) return new KeywordStatement(child, parent);
-	return new ExpressionStatement(child, parent);
+abstract class Stmt extends Statement {
+	createChild(child: Element) {
+		if (child.getAttribute("key")) return new Declaration(child, this);
+		if (child.getAttribute("keyword")) return new KeywordStatement(child, this);
+		return new ExpressionStatement(child, this);
+	}	
 }
 
 export class ExpressionStatement extends Stmt {
+	compile(): Value {
+		throw new Error("Not implemented.");
+	}
 }
 
 export class KeywordStatement extends Stmt {
+	compile(): Value {
+		throw new Error("Not implemented.");
+	}
 }
+
+const COMPILING = Object.freeze(Object.create(null));
+
+type blockType = "object" | /*"array" |*/ "fn" | "expr" | "";
 
 export class Module extends Stmt {
 	constructor(source: Element) {
@@ -48,12 +38,12 @@ export class Module extends Stmt {
 	get scope(): Scope {
 		return this.#scope;
 	}
-	protected compile(): Value {
+	compile(): Value {
 		for (let stmt of this.content) {
 			if (stmt instanceof Declaration) this.scope.put(stmt.key, stmt);
 		}
 		for (let stmt of this.content) {
-			if (stmt.getValue() == Statement.COMPILING) {
+			if (stmt.getValue() == COMPILING) {
 				throw new Error("compiling cycle");
 			}
 		}
@@ -68,8 +58,8 @@ export class Declaration extends Stmt implements Value {
 		this.key = source.getAttribute("key") || "";
 		let facets = source.getAttribute("facets") || "";
 		this.facets = facets ? facets.split(" ") : EMPTY.array as string[];
-		super.initialize();
 	}
+	#value: Value;
 	key: string;
 	facets: string[];
 	get type(): Type {
@@ -77,31 +67,31 @@ export class Declaration extends Stmt implements Value {
 	}
 	get pure(): any {
 		let value = this.getValue();
-		return value == this ? undefined : value.pure;
+		return value.pure;
 	}
-	protected compile(): Value {
-		let expr = super.compile();
-		if (expr) {
-			if (this.content.length) {
-				return this.scope.notice("warn", "Both content & expression are not allowed.", expr);
-			}
-			return expr;
-		}
-		if (this.content.length) {
-			return compileBlock(this);
-		}
-		return this;
+	compile(): Value {
+		return null;
 	}
+	// protected compile(): Value {
+	// 	let expr = this.compileExpr();
+	// 	if (expr) {
+	// 		if (this.content.length) {
+	// 			return this.scope.notice("warn", "Both content & expression are not allowed.", expr);
+	// 		}
+	// 		return expr;
+	// 	}
+	// 	return compileBlock(this);
+	// }
 }
 
-type blockType = "object" | /*"array" |*/ "fn" | "expr" | "";
-function blockType(stmt: Statement): blockType {
+
+function blockType(content: Statement[]): blockType {
 	let type: blockType = "";
-	for (let content of stmt.content) {
-		if (content instanceof Declaration) {
+	for (let stmt of content) {
+		if (stmt instanceof Declaration) {
 			if (type == "expr") return "fn";
 			type = "object";
-		} else if (content instanceof KeywordStatement) {
+		} else if (stmt instanceof KeywordStatement) {
 			return "fn"
 		} else {
 			if (type) return "fn";
@@ -110,23 +100,38 @@ function blockType(stmt: Statement): blockType {
 	}
 	return type;
 }
+
+	// protected compile(): Value {
+	// 	this.compileStatements();
+	// 	return this.compileExpr();
+	// }
 	// protected compileStatements(): void {
 	// 	for (let stmt of this.content) {
-	// 		if (stmt.getValue() == Statement.COMPILING) {
+	// 		if (stmt.getValue() == COMPILING) {
 	// 			throw new Error("compiling cycle");
 	// 		}
 	// 	}
 	// }
+	// protected compileExpr(): Value {
+	// 	let value = this.source.getAttribute("value") || "";
+	// 	if (!value) return;
+	// 	let compilable = parse(lex(value));
+	// 	return compilable.compile(this.scope);
+	// }
 
-function compileBlock(stmt: Stmt): Value {
-	switch (blockType(stmt)) {
+function compile(stmt: Statement) {
+	return new Impure(undefined, null);
+}
+
+function compileBlock(stmt: Statement): Value {
+	switch (blockType(stmt.content)) {
 		case "object":
 			return compileObject(stmt);
 		case "fn":
 		case "expr":
 		case "":
 	}
-	return stmt as Value;
+	return new Pure(undefined, Object.create(null));
 }
 
 function compileObject(source: Statement): Value {
@@ -144,7 +149,7 @@ function compileObject(source: Statement): Value {
 	}
 	for (let name in object) {
 		let stmt: Declaration = object[name];
-		if (stmt.getValue() == Statement.COMPILING) {
+		if (stmt.getValue() == COMPILING) {
 			source.scope.notice("error", `compilation cycle in "${stmt.key}"`, stmt);
 		}
 	}
