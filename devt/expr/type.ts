8b4a31c5abd052370175a5010d101ea8659d0791
jdh <jdhaley@gmail.com>
2022-04-27 09:03:07 -0400

    Added types.ts to devt/expr

---
+++ devt/expr/type.ts
@@ -0,0 +1,68 @@
+import {Bundle} from "../../api/model.js";
+import {Type, Value} from "../../api/value.js";
+
+import {ContainerType, Contract, Signature, Tuple} from "../../base/type.js";
+import {Pure} from "../../base/pure.js";
+import {Scope, Compilable} from "../../base/compiler.js";
+import {Msg} from "./expr.js";
+import {Declaration} from "./statement.js";
+
+export class TYPE {
+	constructor(value: Compilable[]) {
+		this.value = value;
+	}
+	value: Compilable[];
+	compile(scope: Scope): Type {
+		let types = compileTypes(this.value, scope);
+		return compileType(scope, types);
+	}
+}
+
+function compileType(scope: Scope, types: Type[]): Type {
+	let fn = scope.getType("function") as Contract;
+	let type: Type;
+	for (let i = types.length - 1; i >= 0; i--) {
+		let part = types[i];
+		if (part instanceof Tuple && type) {
+			type = new Signature(fn, null, part, type);
+		} else if (type) {
+			type = new ContainerType(part as Contract, type);
+		}
+	}
+	return type;
+}
+
+function compileTypes(source: Compilable[], scope: Scope): Type[] {
+	let types: Type[] = [];
+	for (let i = 0; i < source.length; i++) {
+		let type: Type;
+		let part = source[i];
+		if (part instanceof Msg && part.value == "^") part = source[++i];
+		if (part instanceof Msg) {
+			type = scope.getType(part.value);
+			//if (!type) return Eval.err(`Type "${part.value}" not defined.`);
+		} else if (part instanceof TUPLE) {
+			type = (part as TUPLE).compile(scope);
+		}
+		types.push(type);
+	}
+	return types;
+}
+
+export class TUPLE {
+	constructor(value: Declaration[]) {
+		this.value = value;
+	}
+	value: Declaration[];
+	compile(scope: Scope): Tuple {
+		let members: Bundle<Value> = Object.create(null);
+		for (let decl of this.value) {
+			debugger; //changed compile() to getValue().
+			let type = decl.getValue();
+			if (type.pure instanceof Type) {
+				members[decl.key] = new Pure(scope.getType("type"), type as any);
+			} else {}
+		}
+		return new Tuple(members);
+	}
+}
